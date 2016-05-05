193da76c7317d20d718a6d8211824b5f29fe8e99
ROOSTERs <surinrao12@gmail.com>
2016-05-05 14:43:11 -0400

    removed extra buttons

--- login.php
+++ login.php
@@ -34,38 +34,27 @@
                 <li class="active"><a href="login.php">Login <span class="sr-only">(current)</span></a></li>
                 <li><a href="register.php">Register</a></li>
                  <li><a href="team.php">Meet The Team</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
                   </ul>
                 </li>
-              </ul>
               <form class="navbar-form navbar-left" role="search">
                 <div class="input-group">
                     <div class="input-group">
-                      <input type="text" class="form-control" placeholder="Search">
+
                       <div class="input-group-btn">
-                      <input type="submit" class="btn btn-success">
+
                       </div>
                     </div>
                 </div>
               </form>
               <form class="navbar-form navbar-right" action="logout.php" method="post">
-                <button class="btn btn-warning">Sign out</button>
+
               </form>
               <ul class="nav navbar-nav navbar-right">
                 <li>
                     <?php
                     require("common.php");
                     $arr = array_values($_SESSION['user']);
-                    echo "<a href='#'>Welcome, " . $arr[1] . "</a>";
+
                     ?>
                 </li>
               </ul>
