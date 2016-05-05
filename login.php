f79c58b8b96ecceac683446a55d686fdece48344
H3110 <zhuoen.dai@System.local>
2016-05-05 15:19:11 -0400

    NO MORE MERGING!!!

--- login.php
+++ login.php
@@ -1,72 +1,4 @@
 <html>
-<<<<<<< HEAD
-=======
-<header>
-</header>
-<body>
-    <style>
-        body {
-            background-size: cover;
-            background-repeat: no-repeat;
-
-            background-image: url(background.jpg);
-
-        }
-    </style>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-             <nav class="navbar navbar-inverse">
-
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-
-
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="login.php">Login <span class="sr-only">(current)</span></a></li>
-                <li><a href="register.php">Register</a></li>
-                 <li><a href="team.php">Meet The Team</a></li>
-                  </ul>
-                </li>
-              <form class="navbar-form navbar-left" role="search">
-                <div class="input-group">
-                    <div class="input-group">
-
-                      <div class="input-group-btn">
-
-                      </div>
-                    </div>
-                </div>
-              </form>
-              <form class="navbar-form navbar-right" action="logout.php" method="post">
-
-              </form>
-              <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <?php
-                    require("common.php");
-                    $arr = array_values($_SESSION['user']);
-
-                    ?>
-                </li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
-        </nav>
-
-</body>
-
->>>>>>> origin/master
 <body>
 <!-- Custom font from Google -->
 <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
@@ -190,7 +122,6 @@
 
 ?>
 
-<<<<<<< HEAD
 <nav class="navbar navbar-default navbar-fixed-top">
           <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -251,7 +182,6 @@
           </div><!-- /.container-fluid -->
         </nav>
 
-=======
 </html>
 <?php
 
@@ -362,7 +292,7 @@
     }
 
 ?>
->>>>>>> origin/master
+
 <!--===================================================-->
 <!-- This is the login form that appears in the browser -->
 <!--===================================================-->
