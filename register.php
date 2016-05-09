622a4c6e72196ca6c4b697e8daae7f67f0321aa5
ROOSTERs <surinrao12@gmail.com>
2016-05-09 09:56:21 -0400

    added some pics and finished register page

--- register.php
+++ register.php
@@ -8,17 +8,17 @@
 
     }
 </style>
-	<!-- Custom font from Google -->
-	<link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
+<!-- Custom font from Google -->
+<link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 	
 
@@ -278,7 +278,29 @@
     }
 
 ?>
-<h1>Register</h1>
+</div> <!-- ends here -->
+
+   
+    
+</form> <!-- -->
+
+<!--===================================================-->
+<!-- This is the login form that appears in the browser -->
+<!--===================================================-->
+<div class="col-md-4" padding='150px'>
+<br><br><br><br>
+<div class="panel panel-success">
+    <div class="panel-heading">
+        <h3 class="panel-title">Register</h3>
+    </div>
+<div class="panel-body">
+
+<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+<div class="grid">
+    <div class="row">
+        <div class="col-lg-12">
+        <div class="input-group">
+          
 <form action="register.php" method="post">
     Username:<br />
     <input type="text" name="username" value="" />
@@ -289,9 +311,19 @@
     Password:<br />
     <input type="password" name="password" value="" />
     <br /><br />
-    <input type="submit" value="Register" />
+    <input type="submit" value="Register" class="btn btn-success center-block">
+            </div>
+           
+        </div>
+        </div>
+    </div>
+</div>
 </form>
-</div> <!-- ends here -->
+
+</div>
+</div>
+<!-- End of the login form that appears in the browser -->
+
 
 
 
