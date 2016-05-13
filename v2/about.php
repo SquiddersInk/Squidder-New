fdfc280ea6e0e871811bb7ace51f79875b6c1326
H3110 <zhuoen.dai@ucc.on.ca>
2016-05-13 12:26:22 -0400

    Update about.php

--- v2/about.php
+++ v2/about.php
@@ -52,6 +52,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
                   <li class="active"><a href="about.php">About Us</a></li>
+                  <li><a href="help.php">Help</a></li>
                 <li><a href="login.php">Login <span class="sr-only">(current)</span></a></li>
                 <li><a href="register.php">Register</a></li>
               </ul>
