b7134d58abaefb312051986dc8a91e2e86bdec01
ROOSTERs <surin.rao@ucc.on.ca>
2016-05-17 15:20:03 -0400

    Banner

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <html>
-<!-- ---------------------------ABOUT PAGE, MAY 17th 2016, SQUIDDER INK-------------------------------------------- -->
+<!-- _________________________________ABOUT PAGE, MAY 17th 2016, SQUIDDER INK___________________________ -->
 	<!-- Custom font from Google -->
 	<link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
 
