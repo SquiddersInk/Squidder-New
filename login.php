c9677ab7970cd7b163e829aad27bf5fb608945a8
Temp <temp@so-8300.local>
2016-05-18 20:30:22 -0400

    added slash to check if path works

--- login.php
+++ login.php
@@ -131,7 +131,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <style>
     body {
-        background: url(IMAGES/bg.jpg);
+        background: url(/IMAGES/bg.jpg);
         background-repeat: no-repeats;
         background-size: cover;
     }
